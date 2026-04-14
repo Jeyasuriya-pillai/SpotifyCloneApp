74b5af1349d6adb0988cf5c5e29adbdb6cea90ce
Jeyasuriya-pillai <jeyasuriyapillai03@gmail.com>
2026-04-14 17:03:46 +0530

    Add files via upload

---
+++ theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.spotify.ui.theme // Yeh line sahi honi chahiye
+
+import androidx.compose.ui.graphics.Color
+
+val SpotifyGreen = Color(0xFF1DB954)
+val SpotifyBlack = Color(0xFF121212)
+val SpotifyDarkGray = Color(0xFF282828)
+val SpotifyLightGray = Color(0xFFB3B3B3)
+val SpotifyWhite = Color(0xFFFFFFFF)
